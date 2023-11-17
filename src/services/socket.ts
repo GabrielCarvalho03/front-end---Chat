6cc62ecae341d2459d4526e2d8cc8d474add3a48
Gabriel <gabriel.carvalho@beelegal.com>
2023-11-17 12:30:57 -0300

    add io link vercel back

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 
 
 
-const socket =   io('https://back-end-chat-socket.vercel.app')
+const socket =   io('https://back-end-chat-socket-production.up.railway.app')
 
 
 export {socket}
